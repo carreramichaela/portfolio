51619d67ce81207cd386252719f02f5a8ddb0614
Michaela Carrera <carrera.michaela104@gmail.com>
2019-11-15 14:05:49 -0500

    setup + pictures

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: 'false',
+    animation_speed: 'slow',
+    animation_easing: 'linear'
+  })
+
+});
