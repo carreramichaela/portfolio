5bce71ea22525a487267bbe8170fc2e42c124f13
Michaela Carrera <carrera.michaela104@gmail.com>
2019-11-19 16:56:43 -0500

    adjusted animation times and views

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
 
   var typed = new Typed(".typed", {
     strings: ["Web Developer.","Student.", "Problem Solver."],
-    typeSpeed: 40,
+    typeSpeed: 60,
+    backSpeed: 50,
     loop: true,
     startDelay: 1000,
     showCursor: false,
